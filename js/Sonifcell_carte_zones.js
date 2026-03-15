a58a75d94d059be4b2c65679a0e1bf7eac34b18f
antfa <afaucheryved@ensc.fr>
2026-03-15 17:05:54 +0100

    margin

--- js/Sonifcell_carte_zones.js
+++ js/Sonifcell_carte_zones.js
@@ -2,6 +2,8 @@
 let carte = document.createElement("table");
 carte.style.borderSpacing="0px";
 carte.style.fontFamily = "Segoe UI, Roboto, Helvetica, Arial, sans-serif";
+carte.style.marginLeft = "25px";
+carte.style.marginTop = "50px";
 //carte.style.color = "#455561";
 carte.style.fontWeight = "bold";
 
@@ -11,7 +13,7 @@ let l2 = document.createElement("tr");
 let l3 = document.createElement("tr");
 
 //fonction d'une zone/case
-function Case(txtContent="", couleur="#455561", txtContentHoover="", txtContentClick=""){ //le dernier paramètre correpoond aux explications qui apparaissent en hoover
+function Case(l= "130px", L= "200px",txtContent="", couleur="#455561", txtContentHoover="", txtContentClick=""){ //le dernier paramètre correpoond aux explications qui apparaissent en hoover
     // si on met rien, c'est une case fantome (pour la structure)
     //attention, on mettera sans doute des elements explicatif en appendChild qui ne sera pas forcement du string, mais des div pas ex.
     //   --> dans ce cas, changer la valeur par default "" en null.
@@ -19,8 +21,8 @@ function Case(txtContent="", couleur="#455561", txtContentHoover="", txtContentC
     isCliked = false; //pour si on a cliqué, le txt reste le même jusqu'au clique suivant
     cv.style.verticalAlign = "top";
     cv.style.textAlign = "left";
-    cv.style.width = "200px";
-    cv.style.height = "130px";
+    cv.style.width = L;
+    cv.style.height = l;
     cv.textContent = txtContent;
     cv.style.backgroundColor = couleur;
     cv.addEventListener('mouseenter', function() {
@@ -57,11 +59,11 @@ function Case(txtContent="", couleur="#455561", txtContentHoover="", txtContentC
 }
 
 //Creations zones
-let parametre = Case("parametre", "#954141", "exemple exemple exemple", "tu as cliqué \n truc \n autre truc");
-let userInteraction = Case("user interaction", "#954141", "exemple exemple exemple");
-let dataInput = Case("data input", "#954141", "exemple exemple exemple");
-let sound = Case("sound", "#5E6FA7", "exemple exemple exemple");
-let mapping = Case("mapping", "#4CB081", "exemple exemple exemple");
+let parametre = Case("171px", "553px","parametre", "#954141", "exemple exemple exemple", "tu as cliqué \n truc \n autre truc");
+let userInteraction = Case("171px", "553px","user interaction", "#954141", "exemple exemple exemple");
+let dataInput = Case("301px", "251px", "data input", "#954141", "exemple exemple exemple");
+let sound = Case("301px", "251px", "sound", "#5E6FA7", "exemple exemple exemple");
+let mapping = Case("301px", "552px", "mapping", "#4CB081", "exemple exemple exemple");
 
 // pack :
 l1.appendChild(Case());
@@ -81,5 +83,5 @@ carte.appendChild(l2);
 carte.appendChild(l3);
 
 //à modifier pour le merge
-document.body.appendChild(carte);
-document.body.style.backgroundColor = "#455561";
+let main = document.querySelector(".content");
+main.appendChild(carte);
